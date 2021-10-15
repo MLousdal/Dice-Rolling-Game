f91696e75d8497ac6fb4b75b8a6f9ab10c0d46ac
Mads Lousdal <74186479+MLousdal@users.noreply.github.com>
2021-10-15 12:58:29 +0200

    Hold working

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -22,7 +22,7 @@ const player2Area = document.querySelector("#player2");
 
 let currentPlayer = player1;
 let ScoreEl = player1Area.querySelector("#playerScore");
-let CurrentEl = player1Area.querySelectorAll("#currentScore");
+let CurrentEl = player1Area.querySelector("#currentScore");
 
 function rollDice() {
   let sides = 6;
@@ -31,17 +31,26 @@ function rollDice() {
   let newScore = Number(score) + Number(roll);
 
   if (roll == 1) {
-    newScore = 0;
-    ScoreEl.innerText = newScore;
+    let reset = 0;
+    ScoreEl.innerText = reset;
+    currentPlayer.held = reset;
+    CurrentEl.innerText = reset;
+    console.log(
+      `roll: ${roll}`,
+      `newScore: ${newScore}`,
+      `player: ${currentPlayer.player}`
+    );
+    updateScore(reset);
     switchPlayer();
+    return;
   }
-
-  updateScore(newScore);
   console.log(
     `roll: ${roll}`,
     `newScore: ${newScore}`,
     `player: ${currentPlayer.player}`
   );
+
+  updateScore(newScore);
 }
 
 function updateScore(newScore) {
@@ -54,15 +63,29 @@ function switchPlayer() {
     case 1:
       currentPlayer = player2;
       ScoreEl = player2Area.querySelector("#playerScore");
-      CurrentEl = player2Area.querySelectorAll("#currentScore");
+      CurrentEl = player2Area.querySelector("#currentScore");
       break;
     case 2:
       currentPlayer = player1;
       ScoreEl = player1Area.querySelector("#playerScore");
-      CurrentEl = player1Area.querySelectorAll("#currentScore");
+      CurrentEl = player1Area.querySelector("#currentScore");
       break;
   }
 }
 
+function holdScore() {
+  switch (currentPlayer.player) {
+    case 1:
+      player1.held = player1.score;
+      CurrentEl.innerText = player1.held;
+      break;
+    case 2:
+      player2.held = player2.score;
+      CurrentEl.innerText = player2.held;
+      break;
+  }
+  switchPlayer();
+}
+
 rollBtn.addEventListener("click", rollDice);
-// holdBtn.addEventListener("click", holdScore);
+holdBtn.addEventListener("click", holdScore);
